66a07b30b42ab7abcb66972b3bfdc465a01d1ce1
JinLZhu <rains_lovelily_only@126.com>
2024-12-27 09:15:32 +0800

    Continue...

--- PyGame/Aircraft/AirCraft/game.py
+++ PyGame/Aircraft/AirCraft/game.py
@@ -1,1 +1,12 @@
 # 游戏类Game, 对游戏显示和控制起主要作用的类
+import pygame
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((480, 852))
+        pygame.display.set_caption("AirCraft")
+
+        icon = pygame.image.load("res/image/icon.ico")
+        pygame.disply.set_icon(icon)
